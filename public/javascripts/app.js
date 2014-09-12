2f3f1b19826a4424e795bf24845b439edc8511fd
Gemma <gemmahaylett@gmail.com>
2014-09-11 17:52:55 -0700

    Used jQuery to populate the colleciton view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -92,6 +92,47 @@
 })();
 require.register("scripts/app", function(exports, require, module) {
 require("./landing");
+require("./collection");
+});
+
+;require.register("scripts/collection", function(exports, require, module) {
+var buildAlbumThumbnail = function() {
+    var template =
+        '<div class="collection-album-container col-md-2">'
+      + '  <img src="/images/album-placeholder.png"/>'
+      + '  <div class="caption album-collection-info">'
+      + '    <p>'
+      + '      <a class="album-name" href="/album.html"> Album Name </a>'
+      + '      <br/>'
+      + '      <a href="/album.html"> Artist name </a>'
+      + '      <br/>'
+      + '      X songs'
+      + '      <br/>'
+      + '      <span>X:XX Total Length</span>'
+      + '    </p>'
+      + '  </div>'
+      + '</div>';
+ 
+   return $(template);
+ };
+
+var updateCollectionView = function(){
+  var $collection = $(".collection-container .row");
+  $collection.empty();
+
+  for (var i = 0; i < (Math.random() * 100 )+ 25; i++){
+      var $newThumbnail = buildAlbumThumbnail();
+      $collection.append($newThumbnail);
+    }
+};
+
+if (document.URL.match(/\/collection.html/)) {
+  // Wait until the HTML is fully processed.
+  $(document).ready(function() {
+    console.log("collection.js");
+    updateCollectionView();
+  });
+}
 });
 
 ;require.register("scripts/landing", function(exports, require, module) {
@@ -138,4 +179,3 @@ $(document).ready(function(){
 
 ;
 //# sourceMappingURL=app.js.map
-
