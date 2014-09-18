6533cb57e386d5074370dd93934249116717df3d
Gemma <gemmahaylett@gmail.com>
2014-09-17 20:27:34 -0700

    Converted Bloc Jams to a single page application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,26 @@
 //require('./album');
 //require('./profile');
 
-angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+blocJams = angular.module('BlocJams', ['ui.router']);
+
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+   $locationProvider.html5Mode(true);
+ 
+   $stateProvider.state('landing', {
+     url: '/',
+     controller: 'Landing.controller',
+     templateUrl: '/templates/landing.html'
+   });
+
+   $stateProvider.state('song', {
+     url: '/song',
+     controller: 'Landing.controller',
+     templateUrl: '/templates/song.html'
+   });
+ }]);
+ 
+ // This is a cleaner way to call the controller than crowding it on the module definition.
+ blocJams.controller('Landing.controller', ['$scope', function($scope) {
   $scope.subText = "Turn the music up!";
   $scope.headingText = "Bloc Jams"
 
@@ -31,7 +50,7 @@ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', funct
       for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
       return o;
     };
-    
+
     shuffle($scope.albumURLs);
   }
 
